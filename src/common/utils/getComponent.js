fea07e186bcec3b85b3c77b11bfed12123f761d4
Sankara rao Killi <sankara.killi@materialplus.io>
2023-04-13 18:23:55 +0530

    Added

---
+++ src/common/utils/getComponent.js
@@ -0,0 +1,31 @@
+import Faq from 'src/components/faq/Faq'
+import Hero from 'src/components/hero/Hero'
+import QuickLinks from 'src/components/quick-links/QuickLinks'
+import ShortDescription from 'src/components/short-description/ShortDescription'
+
+const MODULES = {
+  QUICK_LINKS: 'quick_links',
+  SHORT_DESCRIPTION: 'short_description',
+  FAQ: 'faq',
+  HERO: 'hero',
+}
+
+Object.freeze(MODULES)
+
+export function getComponent(acf_fc_layout) {
+  switch (acf_fc_layout) {
+    case MODULES.QUICK_LINKS:
+      return ({ data }) => <QuickLinks key={data.section_id} data={data} />
+    case MODULES.SHORT_DESCRIPTION:
+      return ({ data }) => (
+        <ShortDescription key={data.section_id} data={data} />
+      )
+    case MODULES.FAQ:
+      return ({ data }) => <Faq key={data.section_id} data={data} />
+    case MODULES.HERO:
+      return ({ data }) => <Hero key={data.section_id} data={data} />
+    default:
+      break
+  }
+  return null
+}
